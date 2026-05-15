ee79023a1258136eaa95d81b3827a56e7eda91de
vinoothnamukkamalachowdary-coditas <vinoothnamukkamala.chowdary@coditas.com>
2026-05-15 10:29:53 +0530

    first commit

---
+++ src/main/java/com/example/Dine_Circle_Platform/DineCirclePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dine_Circle_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineCirclePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineCirclePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dine_Circle_Platform/DineCirclePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dine_Circle_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineCirclePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
